988742ce257374251e50bba2ef55367ab3e8e5d7
Jim D Amenakpor <jimdela@yahoo.com>
2025-08-12 15:33:17 -0700

    Update AWS2023Sonarqube7.sh

--- AWS2023Sonarqube7.sh
+++ AWS2023Sonarqube7.sh
@@ -2,16 +2,16 @@
 sudo yum update -y
 sudo yum install wget tree git -y
 cd /opt
-sudo yum install java-11-amazon-corretto -y
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.8.zip
-sudo unzip sonarqube-7.8.zip
+sudo yum install java-21-amazon-corretto -y
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-25.8.0.112029.zip
+sudo unzip sonarqube-25.8.0.zip
 sudo useradd sonar
 
 echo 'sonar ALL=(ALL) NOPASSWD:ALL' | EDITOR='tee -a'  visudo
 
 sudo chown -R sonar:sonar /opt/sonarqube-7.8/
-sudo chmod 755 /opt/sonarqube-7.8
-sudo ln -s /opt/sonarqube-7.8 /opt/sonarqube
+sudo chmod 755 /opt/sonarqube-25.8
+sudo ln -s /opt/sonarqube-25.8 /opt/sonarqube
 
 echo '''vm.max_map_count=524288
 fs.file-max=131072
